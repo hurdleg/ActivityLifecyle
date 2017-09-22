ff1d0a4fb3282f2f9f152f1975e77482aa91503f
Gerald Hurdle <hurdleg@algonquincollege.com>
2017-09-21 21:12:56 -0400

    Documented life-cycle methods in JavaDoc

--- app/src/main/java/com/algonquincollege/hurdleg/activitylifecyle/MainActivity.java
+++ app/src/main/java/com/algonquincollege/hurdleg/activitylifecyle/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     // TextView to display the lifecyle
     private TextView activityLifecyle;
 
+    /**
+     * Called when the activity is first created.
+     * This is where you should do all of your normal static set up: create views,
+     * bind data to lists, etc.
+     * This method also provides you with a Bundle containing the activity's previously frozen state,
+     * if there was one.
+     * Always followed by onStart().
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
     // Remaining lifecycle method overrides (in alphabetical order)
 
+    /**
+     * The final call you receive before your activity is destroyed.
+     * This can happen either because the activity is finishing (someone called finish() on it,
+     * or because the system is temporarily destroying this instance of the activity to save space.
+     * You can distinguish between these two scenarios with the isFinishing() method.
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -53,6 +67,15 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText( getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT ).show();
     }
 
+    /**
+     * Called when the system is about to start resuming a previous activity.
+     * This is typically used to commit unsaved changes to persistent data, stop animations and
+     * other things that may be consuming CPU, etc. Implementations of this method must be very
+     * quick because the next activity will not be resumed until this method returns.
+     *
+     * Followed by either onResume() if the activity returns back to the front,
+     * or onStop() if it becomes invisible to the user.
+     */
     @Override
     public void onPause() {
         super.onPause();
@@ -62,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText( getApplicationContext(), "onPause()", Toast.LENGTH_SHORT ).show();
     }
 
+    /**
+     * Called after your activity has been stopped, prior to it being started again.
+     * Always followed by onStart()
+     */
     @Override
     public void onRestart() {
         super.onRestart();
@@ -71,6 +98,11 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText( getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT ).show();
     }
 
+    /**
+     * Called when the activity will start interacting with the user. At this point your activity
+     * is at the top of the activity stack, with user input going to it.
+     * Always followed by onPause().
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -80,6 +112,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText( getApplicationContext(), "onResume()", Toast.LENGTH_SHORT ).show();
     }
 
+    /**
+     * Called when the activity is becoming visible to the user.
+     * Followed by onResume() if the activity comes to the foreground, or onStop() if it becomes hidden.
+     */
     @Override
     public void onStart() {
         super.onStart();
@@ -89,6 +125,13 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText( getApplicationContext(), "onStart()", Toast.LENGTH_SHORT ).show();
     }
 
+    /**
+     * Called when the activity is no longer visible to the user, because another activity has been
+     * resumed and is covering this one. This may happen either because a new activity is being
+     * started, an existing one is being brought in front of this one, or this one is being destroyed.
+     * Followed by either onRestart() if this activity is coming back to interact with the user,
+     * or onDestroy() if this activity is going away.
+     */
     @Override
     public void onStop() {
         super.onStop();
